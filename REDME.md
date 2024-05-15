df22a87974a562330b7e42e5fd32a2adb8d4ac01
uniquadev <58218460+uniquadev@users.noreply.github.com>
2024-05-15 22:27:24 +0200

    first commit

---
+++ REDME.md
@@ -0,0 +1,27 @@
+# int3_nopper
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/uniquadev/int3_nopper/blob/main/LICENSE)
+
+## Description
+
+int3_nopper is a Binary Ninja plugin that allows you to nop stubbed branches with int3 instructions, which stop functions analysis on Binary Ninja. (ty byfron)
+
+### Features
+
+- Nop int3 instructions to prevent function analysis interruption
+
+### Installation
+
+1. Get in your Binary Ninja plugins directory
+
+2. Drop builded plugin
+
+### Usage
+
+1. Open your target binary in Binary Ninja.
+2. Navigate to the plugin button.
+3. Click on the "Patch int3 instructions" button.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
---
+++ src/main.cpp
@@ -0,0 +1,128 @@
+// (c) 2011 uniquadev
+// This code is licensed under MIT license
+
+#include <cinttypes>
+#include <binaryninjaapi.h>
+#include <thread>
+#include <format>
+
+using namespace BinaryNinja;
+
+class Int3Nopper {
+public:
+	Int3Nopper(BinaryView* view)
+	{
+		this->view = view;
+		arch = view->GetDefaultArchitecture();
+	}
+
+	static void start(Ref<BinaryView> view)
+	{
+		std::thread([view]() {
+			std::make_unique<Int3Nopper>(view)->run();
+		}).detach();
+	}
+private:
+	Ref<BinaryView> view;
+	Ref<Architecture> arch;
+	int64_t patched = 0;
+
+	void nopInt3(uint64_t ea)
+	{
+		view->Write(ea, "\x90", 1);
+		view->SetCommentForAddress(ea, "int3");
+		patched++;
+	}
+
+	uint64_t handleBlock(BasicBlock* block)
+	{
+		if (block->GetLength() < 1)
+			return 0;
+
+		const auto end = block->GetEnd();
+		auto ea = block->GetStart();
+		
+		uint64_t patches = 0;
+		while (ea < end)
+		{
+			const auto len = view->GetInstructionLength(arch, ea);
+			if (len == 0)
+				return 0;
+
+			if (len == 1)
+			{
+				uint8_t byte = 0;
+				if (!view->Read(&byte, ea, 1))
+					return 0;
+
+				if (byte != 0xCC)
+					goto NEXT_INST;
+					
+				do
+				{
+					nopInt3(ea);
+					patches++;
+					ea++;
+				} while (view->Read(&byte, ea, 1) && byte == 0xCC);
+			}
+			else
+			{
+				NEXT_INST:
+				ea += len;
+			}
+		}
+		return patches;
+	}
+
+	void run()
+	{
+		if (arch->GetName() != "x86_64")
+		{
+			LogError("This plugin only supports x86_64 architecture.");
+			return;
+		}
+
+		const Ref<BackgroundTask> task = new BackgroundTask("Patching int3 instructions", true);
+		const auto& fs = view->GetAnalysisFunctionList();
+		const auto& fsLen = fs.size();
+
+		view->SetAnalysisHold(true);
+		int64_t fsCounter = 0;
+		for (const auto& func : fs)
+		{
+			uint64_t fPatches = 0;
+			for (auto& block : func->GetBasicBlocks())
+			{
+				if (task->IsCancelled())
+					return;
+				fPatches += handleBlock(block);
+			}
+			if (fPatches > 0)
+			{
+				LogInfo("Patched %lld int3 in function %s.", fPatches, func->GetSymbol()->GetRawName().c_str());
+				view->UpdateAnalysis();
+			}
+			fsCounter++;
+			task->SetProgressText(std::format("Patching int3 instructions {}/{}", fsCounter, fsLen));
+		}
+		view->SetAnalysisHold(false);
+		LogInfo("Patched %lld int3 instructions.", patched);
+		task->Finish();
+	}
+};
+
+
+extern "C"
+{
+	BN_DECLARE_CORE_ABI_VERSION
+
+	BINARYNINJAPLUGIN bool CorePluginInit()
+	{
+		PluginCommand::Register(
+			"Patch int3 instructions",
+			"Nops breakpoint instructions in order to allow analysis continuation.",
+			Int3Nopper::start
+		);
+		return true;
+	}
+}
