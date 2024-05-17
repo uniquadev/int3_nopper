7e62b1e0e3c51673723aa472b119bf82c6506307
uniquadev <58218460+uniquadev@users.noreply.github.com>
2024-05-17 18:14:17 +0200

    better code

--- src/main.cpp
+++ src/main.cpp
@@ -48,40 +48,39 @@ private:
 			if (len == 0)
 				return 0;
 
-			if (len == 1)
-			{
-				uint8_t byte = 0;
-				if (!view->Read(&byte, ea, 1))
-					return 0;
-
-				if (byte != 0xCC)
-					goto NEXT_INST;
-				
-				// start reading int3 instructions
-				uint64_t patches = 0;
-				do
-				{
-					patches++;
-					ea++;
-				} while (view->Read(&byte, ea, 1) && byte == 0xCC);
-				
-				// ignore compiler generated int3 instructions
-				int64_t start_ea = ea - patches - 1;
-				if (patches > 7)
-				{
-					LogWarn("Ignoring %lld int3 instructions at %llx", patches, start_ea);
-					return 0;
-				}
-
-				for (uint64_t i = 0; i < patches; i++)
-					nopInt3(start_ea + i);
-				return patches;
-			}
-			else
+			if (len != 1)
 			{
 				NEXT_INST:
 				ea += len;
+				continue;
 			}
+
+			uint8_t byte = 0;
+			if (!view->Read(&byte, ea, 1))
+				return 0;
+
+			if (byte != 0xCC)
+				goto NEXT_INST;
+			
+			// start reading int3 instructions
+			uint64_t patches = 0;
+			do
+			{
+				patches++;
+				ea++;
+			} while (view->Read(&byte, ea, 1) && byte == 0xCC);
+			
+			// ignore compiler generated int3 instructions
+			int64_t start_ea = ea - patches - 1;
+			if (patches > 7)
+			{
+				LogWarn("Ignoring %lld int3 instructions at %llx", patches, start_ea);
+				return 0;
+			}
+
+			for (uint64_t i = 0; i < patches; i++)
+				nopInt3(start_ea + i);
+			return patches;
 		}
 		return 0;
 	}
